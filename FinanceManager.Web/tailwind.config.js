3c7ac20294ab9af6f00ef5657b0832f04b58cf26
koralbit <braulio.coral@koralbit.com>
2023-10-21 03:21:05 -0600

    Add Tailwind

---
+++ FinanceManager.Web/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  enabled: true,
+    content: ["./**/*.{razor,cshtml,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+}
+
